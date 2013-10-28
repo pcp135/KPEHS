ac35ff357a32b91ee1d46b9a963dc93658ef449c
Phil <phil@parsons.uk.com>
2013-10-28 12:29:58 +0100

    downloaded data/ignored

---
+++ download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget -x -nd --load-cookies cookies.txt http://www.kaggle.com/c/expedia-personalized-sort/download/data.zip
+wget -x -nd --load-cookies cookies.txt http://www.kaggle.com/c/expedia-personalized-sort/download/basicPythonBenchmark.zip
+wget -x -nd --load-cookies cookies.txt http://www.kaggle.com/c/expedia-personalized-sort/download/testOrderBenchmark.zip
+wget -x -nd --load-cookies cookies.txt http://www.kaggle.com/c/expedia-personalized-sort/download/randomBenchmark.zip
+
