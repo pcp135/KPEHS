e863e155742502de735ca125cc00b02741d2fd67
Phil <phil@parsons.uk.com>
2013-10-28 12:50:05 +0100

    first steps

--- download.sh
+++ download.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 wget -x -nd --load-cookies cookies.txt http://www.kaggle.com/c/expedia-personalized-sort/download/data.zip
-wget -x -nd --load-cookies cookies.txt http://www.kaggle.com/c/expedia-personalized-sort/download/basicPythonBenchmark.zip
-wget -x -nd --load-cookies cookies.txt http://www.kaggle.com/c/expedia-personalized-sort/download/testOrderBenchmark.zip
-wget -x -nd --load-cookies cookies.txt http://www.kaggle.com/c/expedia-personalized-sort/download/randomBenchmark.zip
-
